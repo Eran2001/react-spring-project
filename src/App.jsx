fd257396bbeab4dc054caecd950f8976031a7d34
Eran <eran.hasarali.2001@gmail.com>
2025-02-01 12:44:48 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="main">
+      <div className="items" id="one">
+        1
+      </div>
+      <div className="items" id="two">
+        2
+      </div>
+      <div className="items" id="three">
+        3
+      </div>
+      <div className="items" id="four">
+        4
+      </div>
+      <div className="items" id="five">
+        5
+      </div>
+      <div className="items" id="six">
+        6
+      </div>
+      <div className="items" id="seven">
+        7
+      </div>
+      <div className="items" id="eight">
+        8
+      </div>
+      <div className="items" id="nine">
+        9
+      </div>
+      <div className="items" id="ten">
+        10
+      </div>
     </div>
   );
 };
